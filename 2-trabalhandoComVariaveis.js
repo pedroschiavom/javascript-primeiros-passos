4be1384c01596276503b8ac34bc79c07234abf65
Pedro Schiavom <pedro_indalecio@hotmail.com>
2020-08-21 23:03:56 -0300

    variaveis, operações e conversões

---
+++ 2-trabalhandoComVariaveis.js
@@ -0,0 +1,7 @@
+console.log("Trabalhando com variáveis");
+
+const idade = 29;
+const nome = "Pedro";
+
+
+console.log(idade);
---
+++ 5-atribuicaoVariaveis.js
@@ -0,0 +1,13 @@
+console.log("Trabalhando com variáveis");
+
+const idade = 29;
+const nome = "Pedro";
+const sobrenome = "Schiavom";
+
+
+console.log(nome+ " " + sobrenome);
+console.log(`Meu nome é ${nome} ${sobrenome}` );
+
+const nomeCompleto = nome + sobrenome;
+console.log(nomeCompleto);
+nomeCompleto = 2;
